838e8faf312f212d437665e38717bdbb9f199ac6
Merkulov Ilya <im@redmadrobot.com>
2016-07-10 22:00:13 +0300

    Created example project

---
+++ Example/TouchIdExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TouchIdExample
+//
+//  Created by Merkulov Ilya on 10.07.16.
+//  Copyright © 2016 Merkulov Ilya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
